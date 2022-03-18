57eaae0ffa7d44c3b82c70bbc84fcfd5191d0e21
Aleksey Vokhmin <vohmina2011@yandex.ru>
2022-03-19 01:56:16 +0300

    Added new navbar animation and alert in IE

--- js.js
+++ js.js
@@ -1,10 +1,33 @@
 $("a").click(function (e) {
     e.preventDefault()
-    let active = $("body").find('.active')
-    active.removeClass('active')
-    active.attr("hidden", true)
-    let element = $($(this).attr('href').toString())
-    element.removeAttr("hidden")
-    element.addClass("active")
+    let body = $("body")
 
+    body.find('.active-link').removeClass('active-link')
+    $(this).addClass('active-link')
+
+    let active = body.find('.active')
+    active.removeClass('active')
+    active.attr('hidden', true)
+
+    let element = $($(this).attr('href').toString())
+    element.removeAttr('hidden')
+    element.addClass('active')
+
+    window.scrollTo(0, 0)
 });
+
+
+// https://stackoverflow.com/a/49986758
+function isIE() {
+    const ua = window.navigator.userAgent; //Check the userAgent property of the window.navigator object
+    const msie = ua.indexOf('MSIE '); // IE 10 or older
+    const trident = ua.indexOf('Trident/'); //IE 11
+
+    return (msie > 0 || trident > 0);
+}
+
+if (isIE()) {
+    alert("Внимание!\n\n" +
+        "Данный сайт не оптимизирован для Internet Explorer," +
+        " рекомендуется сменить браузер на более современный.")
+}
